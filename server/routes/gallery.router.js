092da4879eccc4dbe39e47224c6556f88f6d3574
Bruce McCampbell <mccampbellbruce1@gmail.com>
2024-10-07 03:59:58 -0500

    Edited database name in pool.js to reflect default database. Constructed the back-end GET route. In Gallery.router.js.to fetch all gallery items and send them to the client.
    Imported Pool.js module into Gallery.router.js.
    
    Created function to toggle between image and description.
    
    Added seed data to database.sql and Postico database.
    
    Switched GET route and PUT route in gallery.router.js file.

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,17 +1,36 @@
-const express = require('express');
-const router = express.Router();
-// const pool = require('.../modules/pool.js');
+const express = require('express'); //Imports express to create router
+const router = express.Router(); //New Router connection
+const pool = require('../modules/pool.js'); //import pool module for database
 
-// PUT /gallery/like/:id
-router.put('/like/:id', (req, res) => {
-  const galleryId = req.params.id; 
-  const queryText = require('../modules/pool.js');
-  
-});
-
-// GET /gallery
+// GET gallery to fetch all gellery items from DB.
 router.get('/', (req, res) => {
-  // code here
+  const sqlText = `SELECT * FROM "gallery" ORDER BY "id"`; //SQL query to retrieve all rows from 'gallery' table, ordered by 'id'
+  
+  pool.query(sqlText)//Excecutes SQL query
+  .then((result) => {
+    console.log(`Full result from server:`, result); // Logs the full result returned by the query
+    console.log('result from database:', result.rows); // logs only data from 'gallery' table.
+    res.send(result.rows); //sends the data back as a response
+  })
+  .catch((error) => {
+   console.log(`error getting SELECT gallery query ${sqlText}`, error); //error message if query fails to send
+   res.sendStatus(500); // internal Server error if response fails. 
+    });
+ });
+// PUT route to incriment 'like' count for gallery items.
+router.put('/like/:id', (req, res) => {
+  const galleryId = req.params.id; // gets gallery id from url
+  const queryText = `UPDATE "gallery" SET "likes" = "likes" + 1 WHERE "id" = $1;`; //need SQL query to increment 'likes' column.
+
+  pool.query(queryText, [galleryId]) // executes with 'id'
+  .then(result => {
+    res.sendStatus(200); // Sends 200 when 'like' is updated
+  })
+  .catch(err => {
+    console.error('Error completing UPDATE gallery query', err);
+    res.sendStatus(500); // Sends server error response if query  fails 
+  });
 });
 
-module.exports = router;
+
+module.exports = router; // exports router 
--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require('express'); // Import express to create new server
 const app = express();
-// const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5001;
-const gallery = require('./routes/gallery.router.js');
+const bodyParser = require('body-parser'); // Needed to parse incoming request.bodies
+const PORT = process.env.PORT || 5001;  // Defines port!
+const galleryRouter = require('./routes/gallery.router'); // Imports gallery router.
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(express.json());  // needed for axios requests
-app.use(express.static('build'));
+app.use(express.static('build')); // Static files from build directory
 
 /** ---------- EXPRESS ROUTES ---------- **/
-app.use('/api/gallery', gallery);  // app.use('/api/list', listRouter);
+app.use('/api/gallery', galleryRouter);  // app.use('/api/list', listRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
